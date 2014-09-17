b8da9d643b60692418cc2afb7ec9e6d8f3bddbcd
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-17 16:06:20 -0400

    added pages for each question with description

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,19 @@ require 'pry'
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
   end
+
+  def show
+    @question = Question.find(params[:id])
+
+  end
+
 
   def new
     @questions = Question.new
   end
+
 
   def create
     @questions = Question.create(params.require(:question).permit(:question, :description))
